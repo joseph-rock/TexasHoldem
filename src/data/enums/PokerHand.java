bdaf674f1e88442ea1183b71bbdffe841ed83d60
Joseph Rock <josephrock225@gmail.com>
2022-06-16 22:16:46 -0700

    Init PokerHand enum

---
+++ src/data/enums/PokerHand.java
@@ -0,0 +1,32 @@
+package data.enums;
+
+public enum PokerHand {
+    ROYAL_FLUSH(9, "Royal Flush"),
+    STRAIGHT_FLUSH(8, "Straight Flush"),
+    FOUR_OF_A_KIND(7, "Four of a Kind"),
+    FULL_HOUSE(6, "Full House"),
+    FLUSH(5, "Flush"),
+    STRAIGHT(4, "Straight"),
+    THREE_OF_A_KIND(3, "Three of a Kind"),
+    TWO_PAIR(2, "Two Pair"),
+    PAIR(1, "Pair"),
+    HIGH_CARD(0, "High Card"),
+    INIT(-1, "");
+
+    private final int score;
+    private final String displayString;
+
+    PokerHand(int score, String displayString) {
+        this.score = score;
+        this.displayString = displayString;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return displayString;
+    }
+}
