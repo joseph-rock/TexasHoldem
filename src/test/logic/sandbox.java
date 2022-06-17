9aa6e1b8bde65b58de2737671f8cae3dd0650ad3
Joseph Rock <josephrock225@gmail.com>
2022-06-16 20:50:19 -0700

    Save

--- src/test/logic/sandbox.java
+++ src/test/logic/sandbox.java
@@ -1,6 +1,10 @@
 package test.logic;
 
+import data.Card;
+import data.Deck;
 import data.Player;
+import data.enums.Suite;
+import data.enums.Value;
 import logic.TexasHoldem;
 import presentation.PlayerPanel;
 
@@ -10,9 +14,19 @@ import java.awt.*;
 public class sandbox {
 
     public static void main(String[] args) {
+        drawPlayerPanel();
+    }
+
+    private static void drawPlayerPanel() {
         JFrame frame = new JFrame();
         JPanel main = new JPanel();
         main.setBackground(Color.pink);
+
+        Deck deck = new Deck();
+        Card c1 = deck.getCard(Suite.HEARTS, Value.TWO);
+        Card c2 = deck.getCard(Suite.SPADES, Value.TWO);
+
+        Player player = new Player();
 
         // Configure Frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,7 +34,10 @@ public class sandbox {
 
         PlayerPanel panel = new PlayerPanel();
         panel.initActivePlayerDisplay(new Player());
-        panel.displayWinner();
+//        panel.displayWinner();
+//        panel.displayFold();
+        panel.displayAllIn(150);
+        panel.displayCardBack();
 
         main.add(panel.getRootPanel());
 
