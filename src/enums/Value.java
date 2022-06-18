6f68295924ca0e15e1a34d98e32d7db24353017f
Joseph Rock <josephrock225@gmail.com>
2022-06-17 23:46:33 -0700

    Added an int value for more practical use

--- src/enums/Value.java
+++ src/enums/Value.java
@@ -1,24 +1,30 @@
 package enums;
 
 public enum Value {
-    TWO("two"),
-    THREE("three"),
-    FOUR("four"),
-    FIVE("five"),
-    SIX("six"),
-    SEVEN("seven"),
-    EIGHT("eight"),
-    NINE("nine"),
-    TEN("ten"),
-    JACK("jack"),
-    QUEEN("queen"),
-    KING("king"),
-    ACE("ace");
+    TWO   (2, "two"),
+    THREE (3, "three"),
+    FOUR  (4,"four"),
+    FIVE  (5,"five"),
+    SIX   (6,"six"),
+    SEVEN (7,"seven"),
+    EIGHT (8,"eight"),
+    NINE  (9,"nine"),
+    TEN   (10,"ten"),
+    JACK  (11,"jack"),
+    QUEEN (12,"queen"),
+    KING  (13,"king"),
+    ACE   (14,"ace");
 
     private final String displayString;
+    private final int value;
 
-    Value(String displayString) {
+    Value(int value, String displayString) {
         this.displayString = displayString;
+        this.value = value;
+    }
+
+    public int toInt() {
+        return value;
     }
 
     @Override
