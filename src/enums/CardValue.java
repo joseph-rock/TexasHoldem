9e61821b69a20eaab437729d76684322ac59b752
Joseph Rock <josephrock225@gmail.com>
2022-08-10 21:06:51 -0700

    Removed toString method override and valueString from CardValue enum

--- src/enums/CardValue.java
+++ src/enums/CardValue.java
@@ -1,34 +1,27 @@
 package enums;
 
 public enum CardValue {
-    TWO   (2, "two"),
-    THREE (3, "three"),
-    FOUR  (4,"four"),
-    FIVE  (5,"five"),
-    SIX   (6,"six"),
-    SEVEN (7,"seven"),
-    EIGHT (8,"eight"),
-    NINE  (9,"nine"),
-    TEN   (10,"ten"),
-    JACK  (11,"jack"),
-    QUEEN (12,"queen"),
-    KING  (13,"king"),
-    ACE   (14,"ace");
+    TWO   (2),
+    THREE (3),
+    FOUR  (4),
+    FIVE  (5),
+    SIX   (6),
+    SEVEN (7),
+    EIGHT (8),
+    NINE  (9),
+    TEN   (10),
+    JACK  (11),
+    QUEEN (12),
+    KING  (13),
+    ACE   (14);
 
-    private final String displayString;
     private final int value;
 
-    CardValue(int value, String displayString) {
-        this.displayString = displayString;
+    CardValue(int value) {
         this.value = value;
     }
 
     public int toInt() {
         return value;
-    }
-
-    @Override
-    public String toString() {
-        return displayString;
     }
 }
